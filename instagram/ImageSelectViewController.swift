977295e0b168ae890f6284ec239f93434d6e24a4
ryosuke.ishihara <connect_design_events@yahoo.co.jp>
2020-07-18 15:01:59 +0900

    instagramです。

---
+++ instagram/ImageSelectViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ImageSelectViewController.swift
+//  instagram
+//
+//  Created by ryosuke0820 on 2020/07/18.
+//  Copyright © 2020 ryosuke.ishihara. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+}
---
+++ instagram/PostViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PostViewController.swift
+//  instagram
+//
+//  Created by ryosuke0820 on 2020/07/18.
+//  Copyright © 2020 ryosuke.ishihara. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+}
